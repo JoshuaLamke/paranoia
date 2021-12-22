fb62a248ca4e70020354499af987c2cd58603198
Joshua Lamke <joshua.lamke@gmail.com>
2021-12-21 20:56:57 -0500

    set up basic routing

---
+++ src/Routes.tsx
@@ -0,0 +1,17 @@
+import react from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './modules/Home/Home';
+
+const routes: React.FC<{}> = () => {
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
+        </>
+    )
+}
+
+export default routes;
---
+++ src/modules/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {
+
+}
+
+const Home: React.FC<Props> = () => {
+    return (
+        <>
+            <header>
+                Home
+            </header>
+        </>
+    )
+}
+
+export default Home;
